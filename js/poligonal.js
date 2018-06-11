cc2d443d301e7f02565fd5d075f4ab8187a03bfc
Rafael Gutiérrez <ragutimar@gmail.com>
2018-06-11 19:09:18 +0200

    Add colored balls well placed

--- js/poligonal.js
+++ js/poligonal.js
@@ -2,38 +2,39 @@ $.fn.poligonal = function(n, d)
 {
     return this.each(function()
     {
+        var colors = [ "#ff5733", "#ffbd33", "#dbff33", "#75ff33", "#33ff57", "#33ffbd" ];
         var ctx = this.getContext('2d');
         if (n > 1 && d > 1) {
             // Calculamos la circunferencia exterior
             var w = 30;
-            var a = 2 * Math.PI / n;
-            var l = (d - 1) * w;
-            var r = l / (2 * Math.sin(a / 2));
-            var ap = Math.cos(a / 2) * l;
-            var xo = r + 10;
-            var yo = r + 10;
-            for (i = 2; i <= d; i++) {
+            var pr = w * 0.75;
+            var pi2 = 2 * Math.PI;
+            var pi34 = (Math.PI / 2) + (Math.PI / n);
+            var a = pi2 / n;
+            var l0 = (d - 1) * w;
+            var r0 = l0 / (2 * Math.sin(a / 2));
+            var ap = Math.cos(a / 2) * l0;
+            var xo0 = r0 * Math.cos(pi34) + r0 + 10;
+            var yo0 = r0 * Math.sin(pi34) + r0 + 10;
+            for (i = d; i > 0; i--) {
+                c = colors[(i - 1) % colors.length];
                 l = (i - 1) * w;
                 r = l / (2 * Math.sin(a / 2));
+                var xo = xo0 - (r * Math.cos(pi34));
+                var yo = yo0 - (r * Math.sin(pi34));
                 ap = Math.cos(a / 2) * l;
-                ctx.beginPath();
-                ctx.arc(xo, yo, r, 0, 360, false);
-                ctx.stroke();
                 for (j = 0; j < n; j++) {
-                    var ja = a * (j - 0.5);
+                    var ja = (a * j) + pi34;
                     var jxo = r * Math.cos(ja);
                     var jyo = r * Math.sin(ja);
                     var jxf = r * Math.cos(ja + a);
                     var jyf = r * Math.sin(ja + a);
-                    ctx.beginPath();
-                    ctx.moveTo(xo + jxo, yo + jyo);
-                    ctx.lineTo(xo + jxf, yo + jyf);
-                    ctx.stroke();
-                    for (k = 0; k < d; k++) {
+                    for (k = 0; k < i - 1; k++) {
                         var kx = jxo + (((jxf + 1 - jxo) / (i - 1)) * k);
                         var ky = jyo + (((jyf + 1 - jyo) / (i - 1)) * k);
+                        ctx.fillStyle = (j === 0 && k === 0 ? colors[0] : c);
                         ctx.beginPath();
-                        ctx.arc(xo + kx, yo + ky, w * 0.6 / 2, 2 * Math.PI, false);
+                        ctx.arc(xo + kx, yo + ky, pr / 2, 2 * pi2, false);
                         ctx.fill();
                     }
                 }
